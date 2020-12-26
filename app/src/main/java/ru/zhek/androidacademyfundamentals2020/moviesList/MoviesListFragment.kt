107e84bf1368663fb2c4829f42aa066d364d62b7
Eugene <Zhekfake@gmail.com>
2020-12-26 23:11:46 +0300

    code cleanup

--- app/src/main/java/ru/zhek/androidacademyfundamentals2020/moviesList/MoviesListFragment.kt
+++ app/src/main/java/ru/zhek/androidacademyfundamentals2020/moviesList/MoviesListFragment.kt
@@ -1,63 +1,64 @@
 package ru.zhek.androidacademyfundamentals2020.moviesList
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import ru.zhek.androidacademyfundamentals2020.NetworkChecker
 import ru.zhek.androidacademyfundamentals2020.R
+import ru.zhek.androidacademyfundamentals2020.data.Movie
+import ru.zhek.androidacademyfundamentals2020.data.loadMovies
 import ru.zhek.androidacademyfundamentals2020.databinding.FragmentMoviesListBinding
 import ru.zhek.androidacademyfundamentals2020.movieDetails.MovieDetailsFragment
 
 private const val DEFAULT_SPAN = 1
 
-class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list), NetworkChecker.Listener {
 
     private var _binding: FragmentMoviesListBinding? = null
     private val binding get() = _binding!!
-
-    private val job: Job = Job()
+    private val job = Job()
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.Main + job)
     private lateinit var jobUpdateData: Job
 
+    private var movies: List<Movie> = listOf()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("TagLifecycle", "onViewCreated")
         _binding = FragmentMoviesListBinding.bind(view)
 
-        //TODO
-        Log.d("TagLifecycle", "onViewCreated")
-
-        initListComponent()
-    }
-
-    // TODO
-    private fun initListComponent() {
-        setSpanSizeLookup()
+        NetworkChecker.addSubscriber(this)
+        Log.d("MyTag", "NetworkChecker.isConnected = ${NetworkChecker.isConnected}")
 
         binding.rvMovies.apply {
             setHasFixedSize(true)
             adapter?.setHasStableIds(true)
+        }
+        if (NetworkChecker.isConnected) {
+            binding.ivError.visibility = View.GONE
+            initListComponent()
+        } else {
+            binding.ivError.visibility = View.VISIBLE
+        }
+    }
 
-            adapter = MoviesAdapter(onRecyclerItemClicked()).also {
-                jobUpdateData = scope.launch {
-                    Log.d("MyLog", "coroutine started")
-                    it.updateData(requireContext())
-                }
+    private fun initListComponent() {
+        setSpanSizeLookup()
+
+        binding.rvMovies.apply {
+
+            jobUpdateData = scope.launch {
+                fetchData(requireContext())
+                adapter = MoviesAdapter(movies, onRecyclerItemClicked())
             }
         }
-
-//        (binding.rvMovies.adapter as MoviesAdapter).apply {
-//            jobUpdateData = scope.launch {
-//                updateData(requireContext())
-//            }
-//        }
-
     }
 
     private fun setSpanSizeLookup() {
@@ -71,6 +72,10 @@ class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
                 }
             }
         }
+    }
+
+    private suspend fun fetchData(context: Context) {
+        movies = loadMovies(context)
     }
 
     private fun onRecyclerItemClicked(): MoviesAdapter.OnRecyclerItemClicked {
@@ -95,56 +100,22 @@ class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
     }
 
     override fun onDestroyView() {
-//        TODO
-        Log.d("TagLifecycle", "onDestroyView")
         _binding = null
         jobUpdateData.cancel()
+        NetworkChecker.removeSubscriber(this)
         super.onDestroyView()
     }
 
-    //TODO
-    override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("TagLifecycle", "onCreate")
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        Log.d("TagLifecycle", "onCreateView")
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        Log.d("TagLifecycle", "onViewStateRestored")
-        super.onViewStateRestored(savedInstanceState)
-    }
-
-    override fun onStart() {
-        Log.d("TagLifecycle", "onStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d("TagLifecycle", "onResume")
-        super.onResume()
-    }
-
-    override fun onStop() {
-        Log.d("TagLifecycle", "onStop")
-        super.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d("TagLifecycle", "onSaveInstanceState")
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onDestroy() {
-        Log.d("TagLifecycle", "onDestroy")
-        super.onDestroy()
+    override fun onStateChanged(str: String) {
+        activity?.runOnUiThread {
+            if (NetworkChecker.isConnected) {
+                binding.ivError.visibility = View.GONE
+                initListComponent()
+            } else {
+                binding.ivError.visibility = View.VISIBLE
+            }
+        }
+        super.onStateChanged(str)
     }
 
     companion object {
